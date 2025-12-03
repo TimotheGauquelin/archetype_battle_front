1e579ed100665d6775dbd1432db83cbaccfd1252
TimotheSymbolIt <timothe.gauquelin@symbol-it.fr>
2025-12-03 21:49:28 +0100

    update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = screen.getByText(/page d'accueil/i);
+  expect(headingElement).toBeInTheDocument();
+});
+
+test('renders welcome component', () => {
+  render(<App />);
+  const welcomeElement = screen.getByText(/bienvenue/i);
+  expect(welcomeElement).toBeInTheDocument();
 });
