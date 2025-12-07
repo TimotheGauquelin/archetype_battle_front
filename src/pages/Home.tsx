592feb6541c8fb5652c96738a67e50f4ce859b2d
unknown <timothegauquelin@gmail.com>
2025-12-07 20:10:25 +0100

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4 text-gray-800">Archetype Battle</h1>
+    </div>
+  );
+};
+
+export default Home;
