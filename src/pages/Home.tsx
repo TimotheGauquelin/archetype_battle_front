d4b08da53a26eab05a672de279277c77075e6d1e
unknown <timothegauquelin@gmail.com>
2025-12-08 21:48:36 +0100

    created footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import "../styles/Home.scss";
 import Header from "../components/generic/header/Header";
-import Slider from "../components/generic/header/Slider";
+import Footer from "../components/generic/footer/Footer";
 
 const Home = () => {
   return (
     <div className="flex flex-col">
       <div id="headBlock" className="imgBackground">
         <Header />
+        <Footer />
       </div>
     </div>
   );
