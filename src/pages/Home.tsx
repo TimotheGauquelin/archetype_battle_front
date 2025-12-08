394b1fa3af776594aa91ba6b7d36407e728fb292
unknown <timothegauquelin@gmail.com>
2025-12-08 21:58:29 +0100

    :bento: add jumbotron background image and displayed it on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,19 @@
 import "../styles/Home.scss";
 import Header from "../components/generic/header/Header";
 import Footer from "../components/generic/footer/Footer";
+import PageContent from "../components/generic/PageContent";
 
 const Home = () => {
   return (
     <div className="flex flex-col">
       <div id="headBlock" className="imgBackground">
         <Header />
-        <Footer />
+        <div className="h-[500px]"></div>
       </div>
+      <PageContent>
+        <h1>Home</h1>
+      </PageContent>
+      <Footer />
     </div>
   );
 };
